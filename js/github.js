6e3848a3fca9d0a0ecc78ad4878c273f68f83724
dheerendra <dsingh110096@gmail.com>
2020-03-13 22:28:01 +0530

    done

--- js/github.js
+++ js/github.js
@@ -8,10 +8,10 @@ class Github {
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `https:api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
     const reposResponse = await fetch(
-      `https:api.github.com/users/${user}/repos?per_page=${this.respos_count}&sort=${this.respos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      `https://api.github.com/users/${user}/repos?per_page=${this.respos_count}&sort=${this.respos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
     const profileData = await profileResponse.json();
     const reposData = await reposResponse.json();
